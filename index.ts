c368b9ad9eb84791905509577ee724e7a01583d0
ali bakhsh <ali.jumani7520@gmail.com>
2024-03-28 01:03:57 +0500

    puch my guess game on github

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+//guess game
+import inquirer from "inquirer";
+
+//now declear verible
+let a = Math.floor(Math.random() * 8);
+
+console.log("guess a number between 0 to 8");
+
+while (true) {
+  let input = await inquirer.prompt({
+    name: "guess",
+    type: "number",
+    message: "enter your guess number you want to 0 to 8: ",
+  });
+  //provide code
+  let ans: number = input.guess;
+  if (a == ans) {
+    console.log("congratulation your guess number is correct");
+    break;
+  } else {
+    console.log("sorry you guess wrong number try again");
+  }
+}
