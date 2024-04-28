d230278e0a0ddb6db5147cf9bff5ddbf73b69c25
ali bakhsh <ali.jumani7520@gmail.com>
2024-04-28 12:21:01 +0500

    my firt guessgame

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-#! /usr/bin/env node
+#! /usr/bin/env nodecm
+console.log("\n\twelcome to \' code with Ali Bakhsh jumani\'-guess game\n ");
 //guess game
 import inquirer from "inquirer";
 //now declear verible
-let a = Math.floor(Math.random() * 8);
+let a = Math.floor(Math.random() * 9);
 console.log("guess a number between 0 to 8");
 while (true) {
     let input = await inquirer.prompt({
         name: "guess",
         type: "number",
-        message: "enter your guess number you want to 0 to 8: ",
+        message: "enter your guess number you want to 0 to 9: ",
     });
     //provide code
     let ans = input.guess;
